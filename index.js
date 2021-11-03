a19e59c06bf81a1caeb6b0f7459e7a9e90852fc2
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 11:06:59 +0200

    try to dyploed

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const app = require("./app");
+// const app = require("./app");
 const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use("/", express.static(`./front/dist`));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/front/dist/index.html");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
