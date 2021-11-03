517e668cfb58a52ff8fdbeb22d4bb7d1668defd2
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 11:14:56 +0200

    try to dyploed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.use("/", express.static(`./front/dist`));
+// app.use("/", express.static(`./front/dist`));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/front/dist/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/front/dist/index.html");
+// });
 
-module.exports = app;
+// module.exports = app;
