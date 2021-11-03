3db092272998002deb860e3f38302afbdb849705
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 19:36:24 +0200

    adding server

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // const app = require("./app");
 const port = process.env.PORT || 3000;
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const path = require("path");
-
+const shortenUrlRout = require("./routs/shortenUrlRout");
 app.use(cors());
 
-app.use("/", express.static(`./front/dist`));
+app.use(express.json()); // parses requests as json
 
+app.use("/", express.static(`./front/dist`));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./front/dist/index.html"));
 });
+
+app.use("/short", shortenUrlRout);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
