659973a64d585715eeafe44111f0095fe7c47d5c
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 21:59:57 +0200

    adding isValidHttpUrl function

--- front/app/main.js
+++ front/app/main.js
@@ -8,12 +8,36 @@ const baseUrl = "http://localhost:3000/short/";
 shortenBtnEl.addEventListener("click", postUrl);
 
 async function postUrl() {
-  const response = await axios.post(baseUrl, {
-    url: `${urlInputEl.value}`,
-  });
-  const a = document.createElement("a");
-  a.href = response.data;
-  a.innerText = `${response.data}`;
-  newUrlDivEl.appendChild(a);
-  newUrlDivEl.style.display = "block";
+  try {
+    newUrlDivEl.removeChild(newUrlDivEl.firstChild);
+    if (!isValidHttpUrl(urlInputEl.value)) {
+      newUrlDivEl.innerText = "The URL is not valid";
+      newUrlDivEl.style.display = "block";
+      return;
+    }
+    const response = await axios.post(baseUrl, {
+      url: `${urlInputEl.value}`,
+    });
+    const a = document.createElement("a");
+    a.href = response.data;
+    a.innerText = `${response.data}`;
+    newUrlDivEl.appendChild(a);
+    newUrlDivEl.style.display = "block";
+  } catch (error) {
+    console.log(error.response);
+    alert(error.response.data);
+  }
+}
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    if (string.length >= 1000) {
+      return false;
+    }
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
 }
