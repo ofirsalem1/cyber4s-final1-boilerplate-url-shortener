22bd0d583d69821e5672d412b97ca7a6cc859404
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 20:17:15 +0200

    adding to the database creationDate and redirectCount

--- routs/shortenUrlRout.js
+++ routs/shortenUrlRout.js
@@ -8,23 +8,23 @@ const { json } = require("body-parser");
 const baseUrl = "http://localhost:3000/short/";
 
 router.post("/", (req, res) => {
-  shortUrl = shortid.generate();
+  shortId = shortid.generate();
   longUrl = req.body.url;
   dataBase = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
   for (let key in dataBase) {
-    if (dataBase[key] === longUrl) {
-      return res.send(baseUrl + key);
+    if (dataBase[key].longUrl === longUrl) {
+      return res.send(baseUrl + dataBase[key].shortId);
     }
   }
-  dataBase[shortUrl] = longUrl;
+  const urlObj = { shortId, longUrl, creationDate: new Date(Date.now()), redirectCount: 0 };
+  dataBase[shortId] = urlObj;
   fs.writeFileSync("./db.json", JSON.stringify(dataBase));
-  res.send(baseUrl + shortUrl);
+  res.send(baseUrl + shortId);
 });
 
-router.get("/:shortUrl", (req, res) => {
+router.get("/:shortId", (req, res) => {
   dataBase = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
-  console.log(dataBase[req.params.shortUrl]);
-  res.redirect(301, dataBase[req.params.shortUrl]);
+  res.redirect(301, dataBase[req.params.shortId].longUrl);
   res.end();
 });
 
