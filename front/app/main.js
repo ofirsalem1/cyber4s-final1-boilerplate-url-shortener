a9444c1f34f1b0e330da810ad24302c6fe982e79
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-03 19:34:11 +0200

    adding front request

--- front/app/main.js
+++ front/app/main.js
@@ -1,3 +1,16 @@
 import "./styles/style.css";
-// import { selectComponent } from "./js/selectComponent";
-console.log("ofirr");
+const shortenBtnEl = document.getElementById("shorten-btn");
+const urlInputEl = document.getElementById("url_input");
+const newUrlDivEl = document.getElementById("new-url-div");
+
+const baseUrl = "http://localhost:3000/short/";
+
+shortenBtnEl.addEventListener("click", postUrl);
+
+async function postUrl() {
+  const response = await axios.post(baseUrl, {
+    url: `${urlInputEl.value}`,
+  });
+  newUrlDivEl.innerText = `Your new URL: ${response.data}`;
+  newUrlDivEl.style.display = "block";
+}
