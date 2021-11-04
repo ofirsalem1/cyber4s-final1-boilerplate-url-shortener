1ac8359c2faa4a8fcefd009912297c5afbc1a6a5
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-04 09:48:04 +0200

    try heroku

--- front/app/main.js
+++ front/app/main.js
@@ -3,7 +3,7 @@ const shortenBtnEl = document.getElementById("shorten-btn");
 const urlInputEl = document.getElementById("url_input");
 const newUrlDivEl = document.getElementById("new-url-div");
 
-const baseUrl = "/";
+const baseUrl = "http://localhost:3000/short/";
 
 shortenBtnEl.addEventListener("click", postUrl);
 
